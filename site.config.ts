b2163045a9f78a35459b7b8458a561edba01b060
mecovibre <132189212+mecovibre@users.noreply.github.com>
2023-05-01 20:41:48 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,31 +2,31 @@ import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd1e89e9e42eb4ebf9486ae0374039efc',
+  rootNotionPageId: '46b1339b5334482c9f7706fa89aeadfa',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '2skydev blog',
-  domain: 'blog.2skydev.com',
-  author: '2skydev',
+  name: 'MECO on the Rock',
+  domain: 'mecovibre.vercel.app',
+  author: 'MECO',
 
   // open graph metadata (optional)
-  description: '2skydev blog - developer blog',
+  description: 'MECO on the Rock - 일하고 먹고 사는 이야기입니다.',
 
   // social usernames (optional)
-  // twitter: 'transitive_bs',
-  github: '2skydev',
-  // linkedin: 'fisch2',
-  // newsletter: '#', // optional newsletter URL
+  twitter: 'meco_vibre',
+  // github: '#',
+  // linkedin: '#',
+  newsletter: 'https://dawoom.stibee.com/', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon:
-    'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0d2daa37-61d0-45b6-b333-9a2bd0bdc3ee%2Fprofile_%25E1%2584%2580%25E1%2585%25A9%25E1%2584%2592%25E1%2585%25AA%25E1%2584%258C%25E1%2585%25B5%25E1%2586%25AF_circle.png?table=block&id=d1e89e9e-42eb-4ebf-9486-ae0374039efc&spaceId=2eb5336b-2edb-42d0-bc6c-95d72d4d1b74&width=250&userId=bef10e95-202b-4b6b-9626-7af866b6f9ba&cache=v2',
+    'https://file.notion.so/f/s/0cefe44b-0a91-4239-896a-5cb22da9fc68/favicon.png?id=0e773c7c-cf26-4f1c-a670-a6c63e6f6406&table=block&spaceId=d45fb170-b0a2-43cd-92f4-f9b616f901c5&expirationTimestamp=1683027620692&signature=LqL3bZ2ZZnsI29smCmhtC6k-azs_MRvxeMsFpOyDZVw&downloadName=favicon.png',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -61,7 +61,7 @@ export default siteConfig({
   // -------- custom configs (2skydev) -------------
 
   // date-fns format string
-  dateformat: 'yyyy년 MM월 dd일',
+  dateformat: 'yyyy. MM. dd.',
 
   // post page - hidden properties
   hiddenPostProperties: ['설명', '상태', '최하위 정렬'],
